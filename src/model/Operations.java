8c08e831ec79aa5b08d54c5da6c91d4a950421b6
KennetSanchez <kesasaro@gmail.com>
2021-04-15 23:24:53 -0500

    ArrayList removed. Also the recursion is easier to understand.
    I removed the arraylists to make the program easier to manage.

--- src/model/Operations.java
+++ src/model/Operations.java
@@ -1,82 +1,53 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.Objects;
-
 public class Operations {
 
-	ArrayList<Integer> tower1 = new ArrayList<Integer>();
-	ArrayList<Integer> tower2 = new ArrayList<Integer>();
-	ArrayList<Integer> tower3 = new ArrayList<Integer>();
+	int tower1 = 0;
+	int tower2 = 0;
+	int tower3 = 0;
 
 	int disks = 0;
 	String info = "";
-
-	public Operations(int disks) {
-		this.disks = disks;
-		fillArrays();
+	boolean finished;
+	
+	public Operations() {
 	}
 
 	public String getInfo() {
 		return info;
 	}
 
-	public boolean solve(boolean firstP) {
-		boolean finished = false;
-		boolean first = false;
-		int i = 0;
-
-		if (firstP == true) {
-			first = false;
+	public void solve(int disksP, int first, int temp, int last) {
+		disks = disksP;
+		
+		//Increase size.
+		if (first == 1) {
+			tower1--;
+		} else if (temp == 2) {
+			tower2--;
+		} else if (last == 3) {
+			tower3--;
 		}
 
-		if (first) {
-			// Basic cases.
-			if (disks == 1) {
-				tower3.add(tower1.get(0));
-				tower1.removeAll(tower1);
-			} else if (disks == 2) {
-				tower2.add(tower1.get(0));
-				tower3.add(tower1.get(1));
-				tower3.add(tower2.get(0));
-
-				tower1.removeAll(tower1);
-				tower2.removeAll(tower2);
-			} else if (disks >= 3) {
-				tower3.add(tower1.get(0));
-				tower2.add(tower1.get(1));
-
-				tower1.remove(0);
-				tower1.remove(1);
-			}
+		//Reduce size.
+		if(first == 2) {
+		tower2++;
+		}else if(temp == 1) {
+			tower1++;
+		}else if(last == 3) {
+			tower3++;
 		}
-
-		while (!finished) {
-			if(tower3.size() == disks) {
-				finished = true;
-			}
-			
-			else if (tower1.size() - 1 > i && tower2.size() - 1 > i && tower2.get(i) < tower1.get(i)) {
-				tower2.add(tower1.get(i));
-				tower1.remove(i);
-			} else if (tower1.size() - 1 > i && tower3.size() - 1 > i && tower3.get(i) < tower1.get(i)) {
-				tower3.add(tower1.get(i));
-				tower1.remove(i);
-			} else if (tower3.size() - 1 > i && tower2.size() - 1 > i && tower3.get(i) < tower2.get(i)) {
-				tower3.add(tower2.get(i));
-				tower2.remove(i);
-			}
-
-			info += tower1.size() + " " + tower2.size() + " " + tower3.size() + "\n";
-			i++;
-			finished = solve(first);
+		
+		
+		
+		
+		if (disks == 1) {
+			finished = true;
 		}
-		return finished;
-	}
-
-	public void fillArrays() {
-		for (int i = 0; i < disks; i++) {
-			tower1.add(i);
+		else if(!finished){
+			solve(disks - 1, first, last, temp);
+			System.out.println(tower1 + " " + tower2 + " " + tower3 + "\n");
+			solve(disks - 1, temp, first, last);
 		}
 	}
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,8 +5,8 @@ import model.Operations;
 public class Main {
 
 	public static void main(String[] args) {
-		Operations op = new Operations(3);
-		op.solve(true);
+		Operations op = new Operations();
+		op.solve(4, 1, 2, 3);
 		System.out.println(op.getInfo());
 	}
 
