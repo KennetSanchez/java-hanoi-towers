6e4c4e4f4566ea77794bbd7059c4fb4ef797dd09
KennetSanchez <kesasaro@gmail.com>
2021-04-17 00:52:30 -0500

    Finished.
    The program works perfect. The output it's the wanted, and it's verified. Also, the program takes the input from a file. and shows the output in another file. This files are in the "docs" folder.

--- src/model/Operations.java
+++ src/model/Operations.java
@@ -1,9 +1,8 @@
 package model;
 
-import java.util.ArrayList;
-
 public class Operations {
 
+	//This is because the problem it's with 3 towers.
 	final static int TOWERS_AMOUNT = 3;
 
 	public int[] towers;
@@ -11,28 +10,30 @@ public class Operations {
 	int disks = 0;
 	int total = 0;
 	String info = "";
-	boolean finished;
-	
+
 	final static int TOWER1 = 0;
 	final static int TOWER2 = 1;
 	final static int TOWER3 = 2;
 
-	
 	public Operations(int total) {
 		disks = total;
 
+		//Towers array initialize.
 		towers = new int[TOWERS_AMOUNT];
 		towers[TOWER1] = disks;
 		towers[TOWER2] = 0;
 		towers[TOWER3] = 0;
+		
+		//This prints the basic state of the solution (before the first movement).
+		info += towers[TOWER1] + " " + towers[TOWER2] + " " + towers[TOWER3] + "\n";
 	}
 
 	public String solve(int disksP, int origin, int aux, int target) {
 
 		if (disksP > 0) {
 			solve(disksP - 1, origin, target, aux);
-			towers[origin-1]--;
-			towers[target-1]++;
+			towers[origin - 1]--;
+			towers[target - 1]++;
 			info += towers[TOWER1] + " " + towers[TOWER2] + " " + towers[TOWER3] + "\n";
 			solve(disksP - 1, aux, origin, target);
 		}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,37 +1,55 @@
 package ui;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 
 import model.Operations;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		Operations op;
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
+		int disks = 0;
+
+		String solution = "";
+
+		// Input.
+		BufferedReader br = new BufferedReader(new FileReader("docs/input.txt"));
+
+		// Output file.
+		FileOutputStream fos = new FileOutputStream("docs/output.txt");
+		PrintStream ps = new PrintStream(fos);
+
 		System.out.println("How many attemps are you going to do?");
 		int times = Integer.parseInt(br.readLine());
-		int disks = 0;
-		
-		for(int i = 0; i < times ; i++) {
+
+		for (int i = 0; i < times; i++) {
+			//This makes the program useful if there's another input (different from the input file).
 			System.out.println("How many disks are in the first tower?");
-			disks = Integer.parseInt(br.readLine());;
+			disks = Integer.parseInt(br.readLine());
 			
+
 			op = new Operations(disks);
-			bw.write(op.solve(disks, 1, 2, 3));
-			bw.write("Done. \n" );
+			solution = op.solve(disks, 1, 2, 3);
+
+			System.out.println(solution);		
+			System.out.println();
+			
+			//This makes the output of the program, and the verified output match. If it's the last solution, it won't print the "\n".
+			if(i == times-1) {
+				ps.print(solution);
+			}else {
+				ps.print(solution + "\n");
+			}
 		}
 		
 		br.close();
-		bw.close();
+		ps.close();
 	}
 
 }
